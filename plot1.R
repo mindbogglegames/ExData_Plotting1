63c783453d23f6c4d5c8267ac05cd9de018ba9f9
Christian Johansson <mindbogglegames@gmail.com>
2017-08-08 17:16:17 +0200

    Two out of 4 assignments

---
+++ plot1.R
@@ -0,0 +1,14 @@
+#library(dplyr)
+#household_consumption <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings=c("NA","?"))
+#household_consumption_df = tbl_df(household_consumption)
+#rm(household_consumption)
+
+#with(household_consumption_df, plot(Global_active_power))
+
+#household_consumption_df$Date <- as.Date(household_consumption_df$Date, "%d/%m/%Y")
+
+#household_consumption_day <- subset(household_consumption_df, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+with(household_consumption_day, hist(Global_active_power,xlab="Global Active Power (kilowatts)", col="Red", main="Global Active Power"))
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
