b1348f65a64b271d5fee27c106db07896bcd3315
Christian Johansson <mindbogglegames@gmail.com>
2017-08-09 11:38:51 +0200

    Plot4 and cleaned up plot files

--- plot2.R
+++ plot2.R
@@ -1,15 +1,15 @@
 library(dplyr)
 library(lubridate)
-#household_consumption <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings=c("NA","?"))
-#household_consumption_df = tbl_df(household_consumption)
-#rm(household_consumption)
+household_consumption <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings=c("NA","?"))
+household_consumption_df = tbl_df(household_consumption)
+rm(household_consumption)
 
-#household_consumption_df$Date <- as.Date(household_consumption_df$Date, "%d/%m/%Y")
+household_consumption_df$Date <- as.Date(household_consumption_df$Date, "%d/%m/%Y")
 
-#household_consumption_day <- subset(household_consumption_df, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
-#household_consumption_day$full_time <- ymd_hms(with(household_consumption_day, paste(Date, Time)))
+household_consumption_day <- subset(household_consumption_df, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+household_consumption_day$full_time <- ymd_hms(with(household_consumption_day, paste(Date, Time)))
 
-
+par(mfrow=c(1,1), mar=c(5, 4, 4, 2) + 0.1)
 with(household_consumption_day, plot(full_time, Global_active_power, type="l", ylab = "Global Active Power (kilowatts)", xlab = ""))
 
 dev.copy(png, file="plot2.png", height=480, width=480)
--- plot4.R
+++ plot4.R
@@ -8,14 +8,25 @@ household_consumption_df$Date <- as.Date(household_consumption_df$Date, "%d/%m/%
 
 household_consumption_day <- subset(household_consumption_df, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
 household_consumption_day$full_time <- ymd_hms(with(household_consumption_day, paste(Date, Time)))
+colnames(household_consumption_day)
+par(mfrow=c(2,2), mar=c(4,4,2,1))
 
-par(mfrow=c(1,1), mar=c(5, 4, 4, 2) + 0.1)
+#Plot 1
+with(household_consumption_day, plot(full_time, Global_active_power, type="l", ylab = "Global Active Power (kilowatts)", xlab = ""))
+
+#Plot2
+with(household_consumption_day, plot(full_time, Voltage, type="l", ylab = "Voltage", xlab = "datetime"))
+
+#Plot3
 with(household_consumption_day, {
     plot(full_time,Sub_metering_1,ylab = "Energy sub metering", xlab = "", type="l")
     lines(full_time,Sub_metering_2,col='red')
     lines(full_time, Sub_metering_3,col='blue')
-    legend("topright", legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col=c("black", "red", "blue"), lty = 1,  lwd=3)
+    legend("topright", legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col=c("black", "red", "blue"), lty = 1,  lwd=2, bty="n")
 })
 
-dev.copy(png, file="plot3.png", height=480, width=480)
+#Plot4
+with(household_consumption_day, plot(full_time, Global_reactive_power, type="l", ylab = "Global_reactive_power", xlab = "datetime"))
+
+dev.copy(png, file="plot4.png", height=480, width=480)
 dev.off()
